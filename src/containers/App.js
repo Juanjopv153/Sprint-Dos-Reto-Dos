64638378db045ce0d421cd3107eb08a31cf98737
juanjopv153 <juanjopv153@gmail.com>
2022-03-29 21:27:56 -0500

    Comienzo de maquetación con el proyecto

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+import TemporizerContainer from "./TemporizerContainer";
+function App() {
+  return (
+    <div className="App">
+      <h1>WE'RE LAUCHING SOON</h1>
+      <TemporizerContainer/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/TemporizerContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TemporizerContainer = () => {
+  return (
+    <div>TemporizerContainer</div>
+  )
+}
+
+export default TemporizerContainer
